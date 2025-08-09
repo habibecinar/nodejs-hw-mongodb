a06ccc15a9a4dac62c148ec1a3fbd4beacf025e7
Habibe Çınar <146085701+habibecinar@users.noreply.github.com>
2025-08-09 20:47:45 +0300

    edited server.js

--- src/server.js
+++ src/server.js
@@ -2,15 +2,15 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import { getAllContactsController } from "./controllers/contactsController.js";
 import contactsRouter from './routes/contacts.js';
+
 export function setupServer() {
   const app = express();
 
   // JSON body parsing
   app.use(express.json());
 // Route
-app.get("/contacts", getAllContactsController);
+
 
 app.use('/contacts', contactsRouter);
   // Enable CORS
