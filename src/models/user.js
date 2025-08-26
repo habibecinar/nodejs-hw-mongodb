34d7a7f88c4963eb3246073399ecab765b4b329f
Habibe Çınar <146085701+habibecinar@users.noreply.github.com>
2025-08-26 19:50:35 +0300

    add user.js file

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+import { model, Schema } from "mongoose";
+
+// Kullanıcı şeması
+const usersSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true, // isim zorunlu
+    },
+    email: {
+      type: String,
+      required: true, // email zorunlu
+      unique: true,   // aynı email ile 2. kez kayıt olamaz
+    },
+    password: {
+      type: String,
+      required: true, // şifre zorunlu
+    },
+  },
+  {
+    timestamps: true, // createdAt & updatedAt otomatik eklenecek
+    versionKey: false, // __v alanı çıkmasın
+  }
+);
+
+// "users" koleksiyonunu oluşturuyoruz
+export const UsersCollection = model("users", usersSchema);
