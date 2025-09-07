b869aef91879e52098f70652751b0a1e29cb64d3
Habibe Çınar <146085701+habibecinar@users.noreply.github.com>
2025-09-07 19:40:46 +0300

    edited

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -5,6 +5,12 @@ dotenv.config(); // en üstte
 
 export const initMongoConnection = async () => {
   try {
+    // Debug: env değişkenlerini logla
+    console.log('MONGODB_USER:', process.env.MONGODB_USER);
+    console.log('MONGODB_PASSWORD:', process.env.MONGODB_PASSWORD);
+    console.log('MONGODB_URL:', process.env.MONGODB_URL);
+    console.log('MONGODB_DB:', process.env.MONGODB_DB);
+
     const uri = `mongodb+srv://${encodeURIComponent(process.env.MONGODB_USER)}:${encodeURIComponent(process.env.MONGODB_PASSWORD)}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 
     await mongoose.connect(uri);
